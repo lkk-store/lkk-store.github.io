b34503da64ef6e6d13ec6b0a0b54a718db71ffca
KK Lai <kklai>
2021-02-05 02:10:26 +0800

    fix things

--- src/script.js
+++ src/script.js
@@ -8,22 +8,43 @@ d3.select("#g-x").on("click", function(){
 
 })
 
+d3.selectAll(".g-count-button").on("click", function(){
+	var el = d3.select(this);
+	var type = el.attr("data-type");
+
+	console.log(type)
+	var count = +d3.select(".g-count").text();
+
+	if (type == "minus") {
+		count -= 1;
+	} else if (type == "add") {
+		count += 1;
+	}
+
+	count = count < 1 ? 1 : count;
+
+	d3.select(".g-count").text(count);
+
+})
+
 d3.select(".g-button").on("click", function(){
 	d3.select(".g-buy-popup").classed("g-active", true);
 
 	var stockid = d3.select("#stock-name").attr("data-id");
 	var size = d3.select("#size").property("value");
 
-	d3.select(".g-bought").property("value", d3.select("#stock-name").text() + " " + stockid + " x " + d3.select("#fname").property("value") + " x " + size)
+	var name = d3.select("#stock-name").text().replace("(起碼三月先有貨)", "");
+
+	d3.select(".g-bought").property("value", name + " " + stockid + " x " + +d3.select(".g-count").text() + " x " + size)
 
 	if (stockid == "003") {
 
 		var amount = size == "小小心意" ? 10 : size == "多多益善" ? 50 : 100;
-		d3.select("#price").property("value", amount*d3.select("#fname").property("value"))
+		d3.select("#price").property("value", "$" + amount*+d3.select(".g-count").text())
 
 	} else {
 
-		d3.select("#price").property("value", d3.select("#fname").property("value")*280)	
+		d3.select("#price").property("value", "$" + d3.select(".g-count").text()*280)	
 	}
 	
 })
@@ -34,23 +55,35 @@ $( document ).ready( function(){
 	    url = 'https://script.google.com/macros/s/AKfycbzyYUxhWKsfDJlVt6JP0Y6A-DB_YBFfTbnyBqAkd9-fWjkfpAxEZVPX/exec'
 
 	$('#submit').on('click', function(e) {
-	  e.preventDefault();
 
-	  d3.select("#submit").classed("g-loading", true);
+		  e.preventDefault();
 
-	  var jqxhr = $.ajax({
-	    url: url,
-	    method: "GET",
-	    dataType: "json",
-	    data: $form.serialize(),
-	    success: function(){ 
+		  var name = d3.select("#name").property("value");
+		  var phone = d3.select("#phone").property("value");
 
-	    	$('#my-form').trigger("reset");
+		  if (phone == "") {
+		  	d3.select("#phone").transition().style("background", "rgba(255,0,0,0.5)").transition().style("background", "rgba(255, 255, 255, 0.8)")
+		  }
 
-	    	d3.select("#submit").classed("g-loading", false);
-	    	d3.select("#my-form").classed("g-hide", true)
-	    	d3.select(".g-submitted").classed("g-hide", false)
-	    }
-	  })
+		  if (name == "") {
+		  	d3.select("#name").transition().style("background", "rgba(255,0,0,0.5)").transition().style("background", "rgba(255, 255, 255, 0.8)")
+		  }
+
+		  d3.select("#submit").classed("g-loading", true);
+
+		  var jqxhr = $.ajax({
+		    url: url,
+		    method: "GET",
+		    dataType: "json",
+		    data: $form.serialize(),
+		    success: function(){ 
+
+		    	$('#my-form').trigger("reset");
+
+		    	d3.select("#submit").classed("g-loading", false);
+		    	d3.select("#my-form").classed("g-hide", true)
+		    	d3.select(".g-submitted").classed("g-hide", false)
+		    }
+		  })
 	})
 })
