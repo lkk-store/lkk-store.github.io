6c010dc685c144a53f7ed9e3a5414bb587cdc446
KK Lai <kklai>
2021-09-01 13:33:02 +0800

    Fix app icon

--- bin/index.js
+++ bin/index.js
@@ -87,7 +87,7 @@ function compile() {
 			out += '<meta charset="utf-8"> \n';
 			out += '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">\n';
 			out += '<link rel="shortcut icon" href="img/lkk-logo-06.webp" />\n';
-			out += '<link rel="apple-touch-icon" sizes="128x128" href="app-icon.webp">\n'
+			out += '<link rel="apple-touch-icon" sizes="128x128" href="app-icon.png">\n'
 			// out += '<meta name="apple-mobile-web-app-capable" content="yes" />\n'
 			out += '<link rel="stylesheet" href="https://use.typekit.net/nri5irx.css">\n';
 			// out += '<link rel="preconnect" href="https://fonts.gstatic.com">\n'
